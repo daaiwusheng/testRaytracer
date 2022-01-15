d87fdeb1faf144a710e5a31eb7409c4611154ba5
YuWang(Steven) <daaiwusheng003@gmail.com>
2022-01-15 16:05:00 +0000

    用了hemi_sphere 进行采样

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ color ray_color(const ray& r, const hittable& world, int depth) {
         return color(0,0,0);
 
     if (world.hit(r, 0.001, infinity, rec)) {
-        point3 target = rec.p + rec.normal + random_unit_vector();
+        point3 target = rec.p + random_in_hemisphere(rec.normal);
         return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
     }
     vec3 unit_direction = unit_vector(r.direction());
@@ -43,7 +43,7 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 int main(int argc, char *argv[]) {
 
     std::ofstream fileTextureMap;
-    fileTextureMap.open("/Users/wangyu/Downloads/test.ppm");
+    fileTextureMap.open("/Users/wangyu/Downloads/test_hemi.ppm");
 
 
     // Image
