90f65f27f8eb1d2033bcc96d3ed9ec97caebac75
YuWang(Steven) <daaiwusheng003@gmail.com>
2022-01-17 22:27:49 +0000

    [integrate_x_sq.cc] Integrating 𝑥2, v3, 这个应该是不对的

--- integrate_x_sq.cpp
+++ integrate_x_sq.cpp
@@ -10,14 +10,14 @@
 #include <stdlib.h>
 
 inline double pdf(double x) {
-    return x;//对于x平方的积分,应该选取
+    return 0.5;
 }
 
 int main() {
     int N = 1000000;
     auto sum = 0.0;
     for (int i = 0; i < N; i++) {
-        auto x = sqrt(random_double(0,4));
+        auto x = random_double(0,2);
         sum += x*x / pdf(x);
     }
     std::cout << std::fixed << std::setprecision(12);
