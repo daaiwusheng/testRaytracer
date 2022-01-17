b19266426e1ac4c6c48a7931781e5903c0c0fd8b
YuWang(Steven) <daaiwusheng003@gmail.com>
2022-01-17 22:08:15 +0000

    Listing 5: [integrate_x_sq.cc] Integrating 𝑥2 with PDF

--- integrate_x_sq.cpp
+++ integrate_x_sq.cpp
@@ -9,12 +9,16 @@
 #include <math.h>
 #include <stdlib.h>
 
+inline double pdf(double x) {
+    return x;//对于x平方的积分,应该选取
+}
+
 int main() {
     int N = 1000000;
     auto sum = 0.0;
     for (int i = 0; i < N; i++) {
-        auto x = random_double(0,2);
-        sum += x*x;
+        auto x = sqrt(random_double(0,4));
+        sum += x*x / pdf(x);
     }
     std::cout << std::fixed << std::setprecision(12);
     std::cout << "I = " << 2 * sum/N << '\n';
